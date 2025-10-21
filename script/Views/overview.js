4851bd9f4345dc2febb46e6eef893857fea81bc6
Big Poo <156303066+ShangAbi2@users.noreply.github.com>
2025-10-21 10:26:19 +0200

    .

--- script/Views/overview.js
+++ script/Views/overview.js
@@ -1,8 +1,10 @@
 function updateOverView(bookIdx){
     const book = model.data.books[bookIdx]
+    if (!model.viewState.overView.editBook){
     document.getElementById("app").innerHTML = /*html*/`
     <h1>${book.title}</h1>
     <div>
+        <button onclick="${editBook = True}">Edit</button>
         <div>Forlag: ${book.publisher}</div>
         <div>Språk: ${book.language}</div>
         <div>Sider: ${book.pages}</div>
@@ -15,4 +17,30 @@ function updateOverView(bookIdx){
         <div>Informasjon: ${book.details}</div>
     </div>
     `
+    storeViewstate()    
+} 
+    
+    else{
+        document.getElementById("app").innerHTML = /*html*/`
+    <h1>${book.title}</h1>
+    <div>
+        <button onclick="${editBook = True}">Edit</button>
+        <div>Forlag: ${book.publisher}</div>
+        <div>Språk: ${book.language}</div>
+        <div>Sider: ${book.pages}</div>
+        <div>ISBN: ${book.isbn}</div>
+        <div>Utgivelsesår: ${book.publisherYear}</div>
+        <div>rating: ${book.rating} av 5</div>
+    </div>
+    <div>
+        <img src="${book.img}" alt="legg til bildet">
+        <div>Informasjon: ${book.details}</div>
+    </div>
+    `
+    }
+}
+
+
+function storeViewstate(){
+    
 }
