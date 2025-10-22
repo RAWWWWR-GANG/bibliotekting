e07a37aa230e676ea21c74ee82ae19f065da214b
Alexandra Kristiansen <alexxa.k@outlook.com>
2025-10-22 10:23:22 +0200

    rydding

--- script/controllers/controller.js
+++ script/controllers/controller.js
@@ -3,31 +3,6 @@ function goToPage(page) {
   updateView();
 }
 
-function startEditBook() {
-  model.viewState.overview.editBook = true;
-  updateView();
-}
-
-function cancelEdit() {
-  model.viewState.overview.editBook = false;
-  updateView();
-}
-
-function saveEditedBook(id) {
-  const b = model.viewState.overview;
-  const index = model.data.books.findIndex(x => x.id === id);
-  if (index !== -1) model.data.books[index] = { ...model.data.books[index], ...b };
-  b.editBook = false;
-  updateView();
-}
-
-function deleteBook(id) {
-  if (confirm("Delete this book?")) {
-    model.data.books = model.data.books.filter(b => b.id !== id);
-    goToPage("home");
-  }
-}
-
 function saveNewBook(){
     const newBook = { ...model.viewState.registerBook };
 
